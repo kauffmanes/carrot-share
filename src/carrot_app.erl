6c4f809141b9bae7b587e5b92cb5597b2bb209f7
Emily Kauffman <kauffmanes@gmail.com>
2018-03-28 08:44:37 -0400

    combined

--- src/carrot_app.erl
+++ src/carrot_app.erl
@@ -5,13 +5,12 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	% Dispatch = cowboy_router:compile([
-	%   {'_', [{"/", hello_handler, []}]}
-	% ]),
 
 	Dispatch = cowboy_router:compile([
-		{'_', [{"/", {priv_file, ?MODULE, "static/index.html"}}]}
+	  {'_', [{"/hello", hello_handler, []}]},
+	  {'_', [{"/", {priv_file, ?MODULE, "static/index.html"}}]}
 	]),
+
 	{ok, _} = cowboy:start_clear(my_http_listener,
 	  [{port, 8080}],
 	  #{env => #{dispatch => Dispatch}}
