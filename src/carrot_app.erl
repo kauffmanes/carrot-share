7b984b39a8efdc599b52148caf7872e25ea5f098
Emily Kauffman <kauffmanes@gmail.com>
2018-03-28 09:17:16 -0400

    combined

--- src/carrot_app.erl
+++ src/carrot_app.erl
@@ -8,9 +8,7 @@ start(_Type, _Args) ->
 
 	Dispatch = cowboy_router:compile([
 	  {'_', [
-	  	{"/", cowboy_static, {priv_dir, carrot, "static/index.html", [
-	  		{mimetypes, cow_mimetypes, all}
-	  	]}}
+	  	{"/", cowboy_static, {priv_file, carrot, "static/index.html"}}
 	  ]}
 	]),
 	{ok, _} = cowboy:start_clear(http,
