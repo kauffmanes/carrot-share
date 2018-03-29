1712c2c179a56af89d465a82ed96b2921c13f8ae
Emily Kauffman <kauffmanes@gmail.com>
2018-03-28 22:33:28 -0400

    added dir

--- src/carrot_app.erl
+++ src/carrot_app.erl
@@ -8,7 +8,7 @@ start(_Type, _Args) ->
 
 	Dispatch = cowboy_router:compile([
 	  {'_', [
-	  	{"/[...]", cowboy_static, {priv_file, carrot, ""}}
+	  	{"/", cowboy_static, {priv_dir, carrot, priv()}}
 	  ]}
 	]),
 	{ok, _} = cowboy:start_clear(http,
